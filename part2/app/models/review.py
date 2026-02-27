0775d22fccab13da551f7c1e8d0bed641a28a64b
MattW-12106 <12106@holbertonstudents.com>
2026-02-27 13:39:40 +1100

    now handles relationship between place and user

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from app.models.base_model import BaseModel
 
-# Define a Review class to represent a review object
+# defines a Review class to represent a review object
 class Review(BaseModel):
     def __init__ (self, id, text, rating, place, user, created_at, updated_at):
         """Initialize a Review instance."""
@@ -11,6 +11,11 @@ class Review(BaseModel):
         self.place = place
         self.user = user
 
-    
+    # defines one to many relationship with Place and User
+    def set_place(self, place):
+        """Set the place associated with this review."""
+        self.place = place
 
-
+    def set_user(self, user):
+        """Set the user associated with this review."""
+        self.user = user
