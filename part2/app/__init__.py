f644c3eb78db78a1d9e6b87165c2d09be5350cc0
“Shan” <liangyongshan33@gmail.com>
2026-02-27 06:17:53 +0000

    register amenity namespace

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.review import api as review_ns
+from app.api.v1.amenities import api as amenities_ns
 
 def create_app():
     app = Flask(__name__)
@@ -10,4 +11,5 @@ def create_app():
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(review_ns, path='/api/v1/reviews/')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     return app
