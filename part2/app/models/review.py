96eeba2edee567c9573ed82ca0ce2a4fd1f62058
TheKasman <kasapidisandrew@gmail.com>
2026-03-03 03:20:19 +0000

    added validation in review

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -6,8 +6,22 @@ class Review(BaseModel):
     def __init__ (self, id, text, rating, place, user, created_at, updated_at):
         """Initialize a Review instance."""
         super().__init__(id, created_at, updated_at)
+
+        #  Text must be non-empty string
+        if not isinstance(text, str) or not text.strip():
+            raise ValueError("Text must be a non-empty string")
         self.text = text
+
+        # Rating must be integer 1-5
+        if not isinstance(rating, int) or not (1 <= rating <= 5):
+            raise ValueError("Rating must be an integer between 1 and 5")
         self.rating = rating
+
+        # user and place must be non-empty
+        if not user:
+            raise ValueError("user_id is required")
+        if not place:
+            raise ValueError("place_id is required")
         self.place = place
         self.user = user
 
