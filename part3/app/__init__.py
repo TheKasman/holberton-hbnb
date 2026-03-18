efce8283fed3adc4e214d849c3ab8962ddac4afe
psmacabulos <patrick.macabulos@gmail.com>
2026-03-18 11:36:48 +1100

    Patrick part 1

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,12 +4,20 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.reviews import api as review_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    api = Api(app, version='1.0', title='HBnB API',
+               description='HBnB Application API', doc='/api/v1/')
+
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
