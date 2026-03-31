49fd1dfb555dfe8c7b04f22ecf84184b7990387a
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-31 14:02:48 +1100

    Added: header, footer, nav bar

---
+++ part4/config.py
@@ -0,0 +1,18 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SECRET_KEY = "supersecretkey" #  TO BE THROWN AWAY SOON POSSIBLY
+    JWT_SECRET_KEY = "supersecretjwtkey" #  TO BE THROWN AWAY SOON POSSIBLY
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///development.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
